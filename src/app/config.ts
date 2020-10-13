ba4b468600db98a30d2b896ce1262e0a5cd0f2fe
Nofar Baranes <nofar@reallygood.co.il>
2020-10-13 16:47:13 +0300

    last minutes changes: adding configurations

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,7 +4,11 @@ interface IConfig {
 	timeoutIfUiTreeNotFound: number;
 	searchInputDelay: number;
 	successConfetti?: boolean;
-	showUnselectedResultsOnSubmit?: boolean;
+
+	// Form configuration
+	showResultsOnSubmit?: boolean; // show result indicator to selected option on submit
+	showUnselectedResultsOnSubmit?: boolean; // show result indicator to unselected option on submit
+	showExplanationOnSubmit?: boolean; // show explanation section on submit
 
 	// UI configuration
 	appSize: number;
@@ -17,7 +21,9 @@ export const config: IConfig = {
 	timeoutIfUiTreeNotFound: 10000,
 	searchInputDelay: 250,
 	successConfetti: true, // TODO: add to quiz settings in teachme-admin
+	showResultsOnSubmit: false, // TODO: add to quiz settings in teachme-admin
 	showUnselectedResultsOnSubmit: false, // TODO: add to quiz settings in teachme-admin
+	showExplanationOnSubmit: false, // TODO: add to quiz settings in teachme-admin
 	appSize: 500,
 	formHeaderMaxHeight: 150,
 };
--- src/app/layout/screens/form-screen/FormFooter.tsx
+++ src/app/layout/screens/form-screen/FormFooter.tsx
@@ -9,95 +9,97 @@ import useFormManager from "../../../hooks/useFormManager";
 import useViewManager from "../../../hooks/useViewManager";
 import MessageContainer from "../../../components/message-container/MessageContainer";
 import { AppAnimation } from "../../../interfaces/walkme-app/walkmeApp.interface";
+import { config } from "../../../config";
 
 interface IFormFooterProps {
-  onSubmitted: () => void;
-  animationConfig: IScreenAnimationConfig;
+	onSubmitted: () => void;
+	animationConfig: IScreenAnimationConfig;
 }
 
 export default function FormFooter({
-  props,
-  formContext,
+	props,
+	formContext,
 }: {
-  props: IFormFooterProps;
-  formContext: IFormContext;
+	props: IFormFooterProps;
+	formContext: IFormContext;
 }) {
-  const formFooter = useRef<HTMLDivElement>(null);
-  const formCTA = useRef<HTMLDivElement>(null);
-  const { onSubmitted, animationConfig } = props;
-  const {
-    selectedAnswers,
-    submitted,
-    questionsLength,
-    currentRouteId,
-    loading,
-    currentQuestion: { explanation },
-  } = formContext;
+	const formFooter = useRef<HTMLDivElement>(null);
+	const formCTA = useRef<HTMLDivElement>(null);
+	const { onSubmitted, animationConfig } = props;
+	const { showExplanationOnSubmit } = config;
+	const {
+		selectedAnswers,
+		submitted,
+		questionsLength,
+		currentRouteId,
+		loading,
+		currentQuestion: { explanation },
+	} = formContext;
 
-  const { calculateScore, isCorrectAnswers } = useFormManager(formContext);
-  const { animateCoreElements } = useViewManager();
+	const { calculateScore, isCorrectAnswers } = useFormManager(formContext);
+	const { animateCoreElements } = useViewManager();
 
-  const isLastQuestion = currentRouteId >= questionsLength;
-  const formCTALabel = isLastQuestion ? "Finish" : "Next";
+	const isLastQuestion = currentRouteId >= questionsLength;
+	const formCTALabel = isLastQuestion ? "Finish" : "Next";
 
-  const formCTATargetLink = isLastQuestion
-    ? `/summary/${calculateScore()}`
-    : `/form/${currentRouteId + 1}/${calculateScore()}`;
+	const formCTATargetLink = isLastQuestion
+		? `/summary/${calculateScore()}`
+		: `/form/${currentRouteId + 1}/${calculateScore()}`;
 
-  useEffect(() => {
-    if (formCTA.current) {
-      animateCoreElements({
-        elements: [formCTA.current],
-        animateClassName: AppAnimation.FadeInUp,
-        timeout: 3200,
-        remove: true,
-      });
-    }
-  }, [submitted]);
+	useEffect(() => {
+		if (formCTA.current) {
+			animateCoreElements({
+				elements: [formCTA.current],
+				animateClassName: AppAnimation.FadeInUp,
+				timeout: 3200,
+				remove: true,
+			});
+		}
+	}, [submitted]);
 
-  useEffect(() => {
-    if (formFooter.current) {
-      animateCoreElements({
-        elements: [formFooter.current],
-        animateClassName: AppAnimation.FadeInUp,
-        timeout: animationConfig.footer,
-      });
-    }
-  }, []);
+	useEffect(() => {
+		if (formFooter.current) {
+			animateCoreElements({
+				elements: [formFooter.current],
+				animateClassName: AppAnimation.FadeInUp,
+				timeout: animationConfig.footer,
+			});
+		}
+	}, []);
 
-  return (
-    <footer ref={formFooter} className="form-footer">
-      {submitted ? (
-        <>
-          <RouteButton
-            linkTo={formCTATargetLink}
-            id="form-action"
-            buttonType={ButtonType.Default}
-            label={formCTALabel}
-          />
-          {explanation && (
-            <MessageContainer
-              className="explanation"
-              subTitle="Explanation"
-              message={explanation}
-              animateConfig={{
-                animateClass: AppAnimation.FadeInDown,
-                timeout: 300,
-              }}
-            />
-          )}
-        </>
-      ) : (
-        <Button
-          id="form-submit"
-          tmButtonType={ButtonType.Default}
-          buttonClicked={onSubmitted}
-          disabled={!Boolean(selectedAnswers.length)}
-          loading={loading}
-        >
-          <span className="btn-label">Submit</span>
-        </Button>
-      )}
-    </footer>
-  );
+	return (
+		<footer ref={formFooter} className="form-footer">
+			{submitted ? (
+				<>
+					<RouteButton
+						linkTo={formCTATargetLink}
+						id="form-action"
+						buttonType={ButtonType.Default}
+						label={formCTALabel}
+					/>
+					{showExplanationOnSubmit && explanation && (
+						<MessageContainer
+							className="explanation"
+							subTitle="Explanation"
+							message={explanation}
+							animateConfig={{
+								animateClass: AppAnimation.FadeInDown,
+								timeout: 300,
+							}}
+						/>
+					)}
+				</>
+			) : (
+				<Button
+					id="form-submit"
+					tmButtonType={ButtonType.Default}
+					buttonClicked={onSubmitted}
+					disabled={!Boolean(selectedAnswers.length)}
+					loading={loading}
+				>
+					<span className="btn-label">Submit</span>
+				</Button>
+			)}
+		</footer>
+	);
 }
